c530762ea7d7a2415ffd579e970b62b72a198c01
Josef Holzmayr <jester@theyoctojester.info>
2024-11-03 17:11:37 +0000

    chore: refactor GitHub Action workflows
    - separate flows for
      - build testing
      - validation
      - demos
      - experimental
    - artifact collection for validation
    
    Signed-off-by: Josef Holzmayr <jester@theyoctojester.info>

---
+++ ci/collect_artifacts.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+
+if [ -z ${BUILDDIR+x} ]; then
+	echo "BUILDDIR is unset, exiting";
+	exit 1;
+fi
+if [ -z ${ARTIFACT_PATH+x} ]; then
+	echo "ARTIFACT_PATH is unset, exiting";
+	exit 1;
+fi
+
+echo "using BUILDDIR: $BUILDDIR"
+echo "using ARTIFACT_PATH: $ARTIFACT_PATH"
+
+mkdir -p $ARTIFACT_PATH
+
+IMAGE_SUBPATH="build/tmp/deploy/images"
+
+for build in $(ls $BUILDDIR); do
+	echo "found build: $build"
+
+	if [ ! -d "$BUILDDIR/$build/$IMAGE_SUBPATH" ]; then
+		echo "no artifacts found"
+		continue
+	fi
+
+	for board in $(ls $BUILDDIR/$build/$IMAGE_SUBPATH); do
+		echo ".. found board $board"
+		mkdir -p $ARTIFACT_PATH/$board
+		cp -fR $BUILDDIR/$build/$IMAGE_SUBPATH $ARTIFACT_PATH/$board
+		cp -fR $BUILDDIR/$build/build/conf $ARTIFACT_PATH/$board
+
+		if [ -d "$BUILDDIR/$build/build/tmp/buildstats" ]; then
+			cp -fR $BUILDDIR/$build/build/tmp/buildstats $ARTIFACT_PATH/$board
+		fi
+	done
+done
+
